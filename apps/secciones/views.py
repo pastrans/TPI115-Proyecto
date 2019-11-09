bd05f2543bbb4fc746a772d1f642ad2fe314b403
pastrans <pastran2526@gmail.com>
2019-11-08 22:57:13 -0600

    Adecuaciones en el html base para la herencia, herencia para secciones administrar, ingreso de las tablas en secciones administrar.html

--- apps/secciones/views.py
+++ apps/secciones/views.py
@@ -6,6 +6,6 @@ from django.contrib.auth.decorators import login_required
 
 def inicio(request):
 	if request.user.is_superuser  and request.user.is_staff :
-		return render(request, 'base/base.html')
+		return render(request, 'secciones/administrar.html')
 	else :
 		return HttpResponse('Acceso denegado')
