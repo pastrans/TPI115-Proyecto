60bf8e9489072fc2210693937c2962c744f6529c
Gabriel Hidalgo <gabrielalejandro84.gh@gmail.com>
2019-11-30 14:41:00 -0600

    gitignore y avance horarios

--- apps/horaClase/urls.py
+++ apps/horaClase/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from apps.horaClase.views import redirectVacio, resHorarioPnal, resHorarioSecc
 
 urlpatterns = [
-
+    path('', redirectVacio, name='redirectVacio'),
+    path('horarioPersonal/<int:idPnal>', resHorarioPnal, name='resHorarioPnal'),
+    path('horarioSeccion/<int:idSecc>', resHorarioSecc, name='resHorarioSecc'),
 ]
---
+++ static/js/personal/personal.js
@@ -0,0 +1,42 @@
+
+function editPnal(idPnal, nombre, apellido, codigo){
+    $("#frmSeccion").attr('action', 'editar/' + idPnal);
+    $("#nombreP").val(nombre);
+    $("#apellidoP").val(apellido);
+    $("#codigoP").val(codigo);
+}
+
+function elimPnal(idPnal){
+    Swal.fire({
+        title: '¿Seguro de desactivar la sección?',
+        text: "Los datos relacionados con la sección ya no serán visibles",
+        type: 'info',
+        showCancelButton: true,
+        confirmButtonText: 'Sí, desactivar',
+        cancelButtonText: 'Cancelar'
+        }).then((result) => {
+        if (result.value) {
+            location.href = '/personal/cambiarEstado/' + idPnal + '/I'
+        }
+    });
+}
+
+function habPnal(idPnal){
+    Swal.fire({
+        title: '¿Seguro de activar la sección?',
+        text: "Los datos relacionados con la sección ahora serán visibles",
+        type: 'info',
+        showCancelButton: true,
+        confirmButtonText: 'Sí, activar',
+        cancelButtonText: 'Cancelar'
+        }).then((result) => {
+        if (result.value) {
+            location.href = '/personal/cambiarEstado/' + idPnal + '/A'
+        }
+    });
+}
+
+function filtrarSeccion(estado){
+    $("#estado").val(estado);
+    $("#frmFiltroSeccion").submit();
+}
