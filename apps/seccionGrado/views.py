af4b4aca44b22faf476e2671068e6e604e18e03a
pastrans <pastran2526@gmail.com>
2019-12-02 01:08:02 -0600

    Se agregaron los archivos base para trabajar con seccionGrado.

--- apps/seccionGrado/views.py
+++ apps/seccionGrado/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from apps.seccionGrado.models import SeccionGrado
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+# Código para mostrar una secciongrado (se esetandarizo que secciongrado es una representación fisica de los salones )
+@login_required
+def resumenSeccionGrado(request):
+    if not request.session['tipo'] == 'P':
+        return redirect('/my')
+    seccionGrados = SeccionGrado.objects.all()
+    data = {'seccionGrados' : seccionGrados}
+    return render(request, 'seccionGrado/administrar.html', data)
+
+@login_required
+# Código para agregar una Grado
+def agregarSeccionGrado(request):
+    if not request.session['tipo'] == 'P':
+        return redirect('/my')
+    if request.method == 'POST':
+        nombre = request.POST['nombre']
+        sg = SeccionGrado(nombre = nombre)
+        sg.save()
+        return redirect('resumenSeccionGrado')
+    return redirect('resumenSeccionGrado')
