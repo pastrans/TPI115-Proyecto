63be92d97e01dadb75de432e4147e95e0b987039
avelarpablo <56003645+avelarpablo@users.noreply.github.com>
2019-12-02 12:34:59 -0600

    Ya lo de permisos
    Poquito a poquito se termina el trabajito

--- apps/permiso/urls.py
+++ apps/permiso/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
 from django.conf.urls import url
-from apps.permiso.views import resumenPermiso, agregarPermiso, cambiarEstado, eliminarPermiso, editarPermiso
+from apps.permiso.views import resumenPermiso, agregarPermiso, cambiarEstado, editarPermiso
 
 urlpatterns = [
     path('', resumenPermiso, name="resumenPermiso"),#Utilizando
     path('agregar', agregarPermiso, name='agregarPermiso'),#Utilizando
     path('cambiarEstado/<int:idPermiso>/<slug:estado>', cambiarEstado, name='cambiarEstado'),#Utilizando
-    path('<int:idPermiso>', editarPermiso, name="editarPermiso"),
-    path('eliminar/<int:idPermiso>', eliminarPermiso, name="eliminarPermiso"),
+    path('editarPermiso/<int:idPermiso>', editarPermiso, name="editarPermiso"),#Utilizando
 ]
--- apps/permiso/views.py
+++ apps/permiso/views.py
@@ -7,37 +7,18 @@ import re
 # Create your views here.
 
 @login_required
-def resumenPermisoAntes(request):
-    #permiso = Permiso.objects.get(idPermiso=request.session['id'])
-    permisos = Permiso.objects.all()
-    modulos = Modulo.objects.all()
-    errores = set()
-    if request.method == 'POST':
-        errores = validarDatos(request.POST["nombre"])
-        if len(errores) == 0:
-            permiso = Permiso(nombre=request.POST["nombre"])
-            permiso.save()
-            modulosPermiso = request.POST.getlist('modulos')
-            for modulo in modulosPermiso:
-                modulo = Modulo.objects.get(idModulo=modulo)
-                permiso.modulo.add(modulo)
-            return redirect('resumenPermiso')
-    data = {'permisos' : permisos, 'modulos': modulos,  'errores' : errores,
-    'editando': False}
-    return render(request, 'permiso/permiso.html', data)
-
-#Funcionando
 def resumenPermiso(request):
     if not request.session['tipo'] == 'P':
         return redirect('/my')
+    modulos = Modulo.objects.all()
     permisos = Permiso.objects.filter(estado = 'A')
     if request.method == 'POST':
         estado = request.POST['estado']
         permisos = Permiso.objects.filter(estado = estado)
-    data = {'permisos' : permisos}
+    data = {'permisos' : permisos, 'modulos': modulos}
     return render(request, 'permiso/permiso.html', data)
 
-#Funcionando
+@login_required
 def agregarPermiso(request):
     if not request.session['tipo'] == 'P':
         return redirect('/my')
@@ -50,7 +31,7 @@ def agregarPermiso(request):
             permiso.modulo.add(modulo)
     return redirect('resumenPermiso')
 
-#Funcionando
+@login_required
 def cambiarEstado(request,idPermiso,estado):
     if not request.session['tipo'] == 'P':
         return redirect('/my')
@@ -60,51 +41,18 @@ def cambiarEstado(request,idPermiso,estado):
     return redirect('resumenPermiso')
 
 @login_required
-def nuevoPermiso(request):
-    data = {}
-    errores = set()
-    if request.method == 'POST':
-        errores = validarDatosr(request.POST["nombre"])
-        if len(errores) == 0:
-            permiso = Permiso(nombre=request.POST["nombre"])
-            permiso.save()
-            modulosPermiso = request.POST.getlist('modulos')
-            for modulo in modulosPermiso:
-                modulo = Modulo.objects.get(idModulo=modulo)
-                permiso.modulo.add(modulo)
-    data = {'errores' : errores}
-    return render(request, 'permiso/permiso.html', data)
-
-@login_required
 def editarPermiso(request, idPermiso):
     permiso = Permiso.objects.get(idPermiso=idPermiso)
     modulos = Modulo.objects.all()
-    data = {'permiso' : permiso, 'modulos' : modulos, 'editando': True}
-    errores = set()
-    if request.method == 'POST':
-        errores = validarDatos(request.POST["nombre"])
-        if len(errores) == 0:
-            permiso.nombre = request.POST["nombre"]
-            permiso.save()
-            permiso.modulo.clear()
-            modulosPermiso = request.POST.getlist('modulos')
-            for modulo in modulosPermiso:
-                modulo = Modulo.objects.get(idModulo=modulo)
-                permiso.modulo.add(modulo)
-            return redirect('resumenPermiso')
-        else:
-            data = {'permiso' : permiso, 'modulos' : modulos,
-            'errores' : errores, 'editando': True}
+    permisos = Permiso.objects.filter(estado = 'A')
+    if request.method == "POST":
+        permiso.nombre = request.POST["nombrePermiso"]
+        modulosPermiso = request.POST.getlist('modulos')
+        permiso.modulo.clear()
+        for modulo in modulosPermiso:
+            modulo = Modulo.objects.get(idModulo=modulo)
+            permiso.modulo.add(modulo) 
+        permiso.save()
+        return redirect('resumenPermiso')
+    data = {'permiso' : permiso, 'modulos' : modulos, 'permisos' : permisos, 'editando': True}
     return render(request, 'permiso/permiso.html', data)
-
-@login_required
-def eliminarPermiso(request, idPermiso):
-    permiso = Permiso.objects.get(idPermiso=idPermiso)
-    permiso.delete()
-    return redirect('resumenPermiso')
-
-def validarDatos(nombre):
-    errores = set()
-    if(not re.match("^[a-zA-ZáéíóúÁÉÍÓÚüÜñÑ0-9 ]+$", nombre)):
-        errores.add('Nombre inválido')
-    return errores
