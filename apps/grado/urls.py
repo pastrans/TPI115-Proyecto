0cf6fdf0bc44a05e3abe41552af560db74064d2c
pastrans <pastran2526@gmail.com>
2019-12-02 00:29:16 -0600

    Eliminar y Editar en grado terminado

--- apps/grado/urls.py
+++ apps/grado/urls.py
@@ -4,5 +4,6 @@ from apps.grado.views import *
 urlpatterns = [
     path('', resumenGrado, name='resumenGrado'),
     path('agregar', agregarGrado, name='agregarGrado'),
-
+    path('editar/<int:idGrado>', editarGrado, name='editarGrado'),
+    path('cambiarEstado/<int:idGrado>/<slug:estado>', estadoGrado, name='estadoGrado'),
 ]
--- apps/grado/views.py
+++ apps/grado/views.py
@@ -14,8 +14,9 @@ def resumenGrado(request):
         grados = Grado.objects.filter(estado=request.POST['estado'])
     data = {'grados' : grados}
     return render(request, 'grado/administrar.html', data)
-# para guardar un registro en la base de datos
 
+
+# para guardar un registro en la base de datos
 @login_required
 def agregarGrado(request):
     if not request.session['tipo'] == 'P':
@@ -26,4 +27,27 @@ def agregarGrado(request):
         grado = Grado(nombre = nombre, nivel = nivel)
         grado.save()
         return redirect('resumenGrado')
+    return redirect('resumenGrado')
+
+# para editar un registro de grado
+@login_required
+def editarGrado(request, idGrado):
+    if not request.session['tipo'] == 'P':
+        return redirect('/my')
+    if request.method == 'POST':
+        grado = Grado.objects.get(idGrado = idGrado)
+        grado.nombre = request.POST['nombre']
+        grado.nivel = request.POST['nivel']
+        grado.save()
+        return redirect('resumenGrado')
+    return redirect('resumenGrado')
+
+# para eliminar un registro de grado (solo de cambia el estado para mantener la integridad de los datos)
+@login_required
+def estadoGrado(request, idGrado, estado):
+    if not request.session['tipo'] == 'P':
+        return redirect('/my')
+    grado = Grado.objects.get(idGrado = idGrado)
+    grado.estado = estado
+    grado.save()
     return redirect('resumenGrado')
